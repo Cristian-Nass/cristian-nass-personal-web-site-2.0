0322269b1e8d716019714a4f99c1cbf5efd9d687
Cristian <cristian.tirdad@gmail.com>
2026-04-02 14:29:45 +0200

    save

--- src/App.tsx
+++ src/App.tsx
@@ -10,19 +10,19 @@ function App() {
   return (
     <main>
       <NavBar />
-      <div className="section-wrapper">
+      <div className="section-wrapper" id="home">
         <HomeSection />
       </div>
-      <div className="section-wrapper">
+      <div className="section-wrapper" id="about">
         <AboutSection />
       </div>
-      <div className="section-wrapper">
+      <div className="section-wrapper" id="education">
         <EducationSection />
       </div>
-      <div className="section-wrapper">
+      <div className="section-wrapper" id="experience">
         <ExperienceSection />
       </div>
-      <div className="section-wrapper">
+      <div className="section-wrapper" id="contact">
         <ContactSection />
       </div>
     </main>
